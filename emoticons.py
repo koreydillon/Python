47231cab781e9cd333f12d2992e39b26c1e1abbc
Korey Dillon <162940716+koreydillon@users.noreply.github.com>
2024-06-28 11:49:57 -0700

    Update emoticons.py

--- emoticons.py
+++ emoticons.py
@@ -1,8 +1,4 @@
 '''
-Name: Korey Dillon
-Date: 06/24/24
-Class: ISTA 130
-Section Leader: N/A
 
 Dictionaries with Lists as Values !!!
 Building functions where this emoticons.py file will successfully execute each function to test with test_fimoticon.py
